5213293d538b533784a39afd971f79490286721b
Ilya Puchka <ilyapuchka@gmail.com>
2017-10-13 03:03:44 +0200

    tests fixed

--- QuandooTests/Data Access Layer/APIPostRepositoryTests.swift
+++ QuandooTests/Data Access Layer/APIPostRepositoryTests.swift
@@ -16,12 +16,12 @@ class APIPostRepositoryTests: XCTestCase {
         let networkSession = FileNetworkSession()
         let data = try Data(resource: R.file.postJson)
         let post = try JSONDecoder().decode(Post.self, from: data)
-        networkSession.responses = [.postsRequest(userId: "1"): [post]]
+        networkSession.responses = [.postsRequest(userId: 1): [post]]
         
-        let sut = APIUserRepository(networkSession: networkSession)
+        let sut = APIPostRepository(networkSession: networkSession)
         
         let expectPosts = expectation(description: "Received posts")
-        sut.getUsers { (posts, error) in
+        sut.getPosts(withUserId: 1) { (posts, error) in
             XCTAssertNil(error)
             XCTAssertNotNil(posts)
             XCTAssertEqual(posts?.count, 1)
@@ -30,6 +30,15 @@ class APIPostRepositoryTests: XCTestCase {
         }
         
         waitForExpectations(timeout: 1.0, handler: nil)
+        
+        let expectNoPosts = expectation(description: "Received no posts")
+        sut.getPosts(withUserId: 0) { (posts, error) in
+            XCTAssertNil(posts)
+            expectNoPosts.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+
     }
     
 }
